cfcdabd1e702eba2c7e02f267ad4cce037580022
Caleb Oki <caleb.oki@ibm.com>
2023-07-05 16:49:39 -0400

    the landing page

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Listing;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Builder;
+
+class ListingController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Listing::query()
+            ->where('is_active', true)
+            ->with('tags')
+            ->latest();
+
+        if ($request->has('s')) {
+            $searchQuery = trim($request->get('s'));
+
+            $query->where(function (Builder $builder) use ($searchQuery) {
+                $builder
+                    ->orWhere('title', 'like', "%{$searchQuery}%")
+                    ->orWhere('company', 'like', "%{$searchQuery}%")
+                    ->orWhere('location', 'like', "%{$searchQuery}%");
+            });
+        }
+
+        if ($request->has('tag')) {
+            $tag = $request->get('tag');
+            $query->whereHas('tags', function (Builder $builder) use ($tag) {
+                $builder->where('slug', $tag);
+            });
+        }
+
+        $listings = $query->get();
+
+        $tags = Tag::orderBy('name')
+            ->get();
+
+        return view('listings.index', compact('listings', 'tags'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controllers\ListingController::class, 'index'])
+    ->name('listings.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/{listing}', [Controllers\ListingController::class, 'show'])
+    ->name('listings.show');
 
 require __DIR__.'/auth.php';
